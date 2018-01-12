533b562c1ecebaa54bcf91854c91aa38613efc40
suryaprabhaponnusamy <35135322+suryaprabhaponnusamy@users.noreply.github.com>
2018-01-12 19:33:02 +0500

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i,N,sum=0;
+printf("enter a number");
+scanf("%d",&N);
+for(i=0;i<=N;i++)
+{
+sum+=0;
+}
+printf("sum of natural number=%d",N,sum);
+return 0;
+}
