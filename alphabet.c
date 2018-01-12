702f14a273d4e390350b296a9f132f124ef9017f
suryaprabhaponnusamy <35135322+suryaprabhaponnusamy@users.noreply.github.com>
2018-01-12 19:49:13 +0500

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char c;
+printf("enter a character");
+scanf("%ch",&c);
+if((c>='a'&&c<='z')||(c>='A'&&C<='Z'))
+{
+printf("%ch is an alphabet",c);
+}
+else
+{
+printf(%ch is not an alphabet");
+}
+return 0;
+}
